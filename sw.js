87cfe5792922e273f8adfdbe66d257966275d41b
Danny Rodriguez <drodri74@uic.edu>
2021-03-12 02:19:57 +0000

    updating cache

--- sw.js
+++ sw.js
@@ -38,10 +38,7 @@ self.addEventListener('fetch', function(event) {
             // to clone it so we have two streams.
             var responseToCache = response.clone();
 
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
+           
 
             return response;
           }
